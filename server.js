b1d05336fd4e33d7c0a6a5c744c02b079b00b47d
Jeffery L. Graham <jeffery.lemar.graham@gmail.com>
2018-11-10 18:21:08 -0500

    add post route for uploadinh files

--- server.js
+++ server.js
@@ -1,27 +1,33 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var cors = require('cors');
+var express = require("express");
+var cors = require("cors");
 
 // require and use "multer"...
-
+var multer = require("multer");
 var app = express();
+var upload = multer({ dest: "uploads/" });
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.post("/api/fileanalyse", async (req, res) => {
-  res.send("Working")
-})
+app.get("/hello", function(req, res) {
+  res.json({ greetings: "Hello, API" });
+});
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+app.post(
+  "/api/fileanalyse",
+  upload.single("upfile"),
+  async (req, res, next) => {
+    var filename = req.file;
+    res.json({name: filename.originalname, type: filename.mimetype, size: filename.size});
+  }
+);
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Node.js listening ...");
 });
